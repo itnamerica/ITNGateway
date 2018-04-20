4421731bbcf07c9421d05474f254376e73df4916
Samantha Guergenenov <samguergen@gmail.com>
2018-04-20 16:41:32 -0400

    fix fatal typo of previous commit fix

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/app'));
 
 var allPages = ['home','what-we-do','organization','faces','faq','news','contact','become-member','member-app','volunteer-to-drive','volunteer-app','family','member-programs','pay-online','donate','corporate'];
 
-app.get(allPages, function(req, res){
+app.get('/'+ allPages, function(req, res){
   res.sendFile(__dirname + '/app/index.html');
 });
 
